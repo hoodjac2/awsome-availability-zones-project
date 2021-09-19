5b6304f730b08d6d67cae1038fbb42fae335df32
Jake Hood <hoodjac2@msu.edu>
2021-09-19 17:20:11 -0400

    Updated with navigation and materials package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-
+import { ListViewComponent } from './list-view/list-view.component';
+import { UploadCsvComponent } from './upload-csv/upload-csv.component';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'upload-csv', component: UploadCsvComponent},
+      {path: 'list-view', component: ListViewComponent},
+      {path: '', redirectTo: '/list-view', pathMatch: 'full'},
+      {path: '**', component: NotFoundPageComponent}
+    ]),
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
