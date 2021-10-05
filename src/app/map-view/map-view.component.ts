241d712fa86f012a6230d890779444e13e7d7225
Jake Hood <hoodjac2@msu.edu>
2021-10-04 20:06:31 -0400

    Implemented a working map view

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,10 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, AfterViewInit } from "@angular/core";
+import * as L from 'leaflet';
 
 @Component({
   selector: 'map-view',
   templateUrl: './map-view.component.html',
   styleUrls: ['./map-view.component.css']
 })
-export class MapViewComponent{
+export class MapViewComponent implements AfterViewInit{
     title = 'capstone-test';
+    private map : any;
+    constructor(){
+
+    }
+    ngAfterViewInit(): void {
+      this.initMap();
+     }
+     private initMap(): void {
+      this.map = L.map('map', {
+        center: [ 39.8282, -98.5795 ],
+        zoom: 3
+      });
+      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+    tiles.addTo(this.map);
+    }
 }
