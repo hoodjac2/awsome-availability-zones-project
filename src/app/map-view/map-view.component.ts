e8e8980d7f4a4769e8cf827df7baddf31843a723
Wynton Huang <huangwyn@msu.edu>
2021-10-06 14:34:50 -0400

    At this point, markers should be displaying

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,5 +1,22 @@
 import { Component, AfterViewInit } from "@angular/core";
 import * as L from 'leaflet';
+import { MarkerService } from '../marker.service';
+
+// This stuff makes marker icons
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
 
 @Component({
   selector: 'map-view',
@@ -9,11 +26,12 @@ import * as L from 'leaflet';
 export class MapViewComponent implements AfterViewInit{
     title = 'capstone-test';
     private map : any;
-    constructor(){
+    constructor(private markerService: MarkerService){
 
     }
     ngAfterViewInit(): void {
       this.initMap();
+      this.markerService.makeLocationMarkers(this.map);
      }
      private initMap(): void {
       this.map = L.map('map', {
--- src/app/marker.service.ts
+++ src/app/marker.service.ts
@@ -1,9 +1,29 @@
+/**
+ * This file does the marker for the maps (in theory)
+ *
+ * started by Wynton Huang - raise any issues with this file with him
+ */
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as L from 'leaflet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MarkerService {
+  locations: string = '/assets/data/test_geo.geojson';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  makeLocationMarkers(map: L.Map): void {
+    this.http.get(this.locations).subscribe((res: any) => {
+      for (const c of res.features) {
+        const lon = c.geometry.coordinates[0];
+        const lat = c.geometry.coordinates[1];
+        const marker = L.marker([lat, lon]);
+
+        marker.addTo(map);
+      }
+    });
+  }
 }
