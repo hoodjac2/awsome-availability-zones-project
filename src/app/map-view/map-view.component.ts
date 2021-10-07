d5fa4ff01f300638b51c911258ae45d3616a6fa8
Jake Hood <hoodjac2@msu.edu>
2021-10-06 21:33:53 -0400

    Updated the map with working markers

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -2,22 +2,6 @@ import { Component, AfterViewInit } from "@angular/core";
 import * as L from 'leaflet';
 import { MarkerService } from '../marker.service';
 
-// This stuff makes marker icons
-const iconRetinaUrl = 'assets/marker-icon-2x.png';
-const iconUrl = 'assets/marker-icon.png';
-const shadowUrl = 'assets/marker-shadow.png';
-const iconDefault = L.icon({
-  iconRetinaUrl,
-  iconUrl,
-  shadowUrl,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  tooltipAnchor: [16, -28],
-  shadowSize: [41, 41]
-});
-L.Marker.prototype.options.icon = iconDefault;
-
 @Component({
   selector: 'map-view',
   templateUrl: './map-view.component.html',
@@ -31,12 +15,15 @@ export class MapViewComponent implements AfterViewInit{
     }
     ngAfterViewInit(): void {
       this.initMap();
-      this.markerService.makeLocationMarkers(this.map);
      }
      private initMap(): void {
       this.map = L.map('map', {
-        center: [ 39.8282, -98.5795 ],
+        center: [37.663711, -78.723905],
         zoom: 3
+      });
+      var regions = this.getRegionMarkers();
+      regions.forEach(regionMarker => {
+        regionMarker.addTo(this.map);
       });
       const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
@@ -45,5 +32,38 @@ export class MapViewComponent implements AfterViewInit{
     });
 
     tiles.addTo(this.map);
+    }
+    private getRegionMarkers(): any[]{
+      var arrayOfRegions = [
+        //Virginia
+        L.circle([38.262715, -78.205075], {
+          color: 'orange',
+          fillColor: '#FFA500',
+          fillOpacity: 0.5,
+          radius: 250000
+        }).bindPopup("I am US-EAST/Virginia."),
+        //Ohio
+        L.circle([40.327178, -83.077396], {
+          color: 'orange',
+          fillColor: '#FFA500',
+          fillOpacity: 0.5,
+          radius: 250000
+        }).bindPopup("I am US-EAST/Ohio."),
+        //North California
+        L.circle([40.226597, -122.148534], {
+          color: 'orange',
+          fillColor: '#FFA500',
+          fillOpacity: 0.5,
+          radius: 250000
+        }).bindPopup("I am US-WEST/CALI."),
+        //Oregon
+        L.circle([43.879583, -120.698427], {
+          color: 'orange',
+          fillColor: '#FFA500',
+          fillOpacity: 0.5,
+          radius: 250000
+        }).bindPopup("I am US-WEST/OREGON."),
+      ];
+      return arrayOfRegions;
     }
 }
